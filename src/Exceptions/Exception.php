4940dc4576e4c3ed932e274d184f890745cd2924
Kekalainen <git@kekalainen.me>
2023-09-22 12:21:05 +0000

    Add exception wrap method

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -3,7 +3,27 @@
 namespace Kekalainen\GameRQ\Exceptions;
 
 use Exception as BaseException;
+use Throwable;
 
 abstract class Exception extends BaseException
 {
+    /**
+     * Executes the given callable and rethrows any throwable
+     * as an instance of this exception class.
+     *
+     * @return mixed the value returned by the callable.
+     * @throws static if the callable throws.
+     */
+    public static function wrap(callable $callable)
+    {
+        try {
+            return $callable();
+        } catch (Throwable $throwable) {
+            throw new static(
+                $throwable->getMessage(),
+                (int) $throwable->getCode(),
+                $throwable
+            );
+        }
+    }
 }
